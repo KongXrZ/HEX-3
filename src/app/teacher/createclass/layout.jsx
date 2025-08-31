b72e22f2da1b371f4512a1a68cbf71901d857183
asnaweevixvify <asnaweeezor@gmail.com>
2025-08-31 09:03:48 +0700

    file

---
+++ src/app/teacher/createclass/layout.jsx
@@ -0,0 +1,8 @@
+export default function CreateClassLayout({ children }) {
+    return (
+      <div style={{ backgroundColor: "#E8E8E8",width:"100vw",height:"100vh"}}>
+        {children}
+      </div>
+    );
+  }
+  
